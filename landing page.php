0a7f862dd0c01dce970a7d7f807ef3dbfc05aff3
PRIYANKA DAS <priyankadas20056a@gmail.com>
2025-09-05 11:45:43 +0530

    Add files via upload

---
+++ landing page.php
@@ -0,0 +1,8 @@
+<?php
+ 
+ 
+    echo " PATIENT OR PATIENT'S GUARDIAN  NAME IS :- " . $_POST['name']."<br>";
+    echo " PATIENT OR PATIENT'S GUARDIAN EMAIL IS :- ".$_POST['email']."<br>";
+    echo " PATIENT OR PATIENT'S GUARDIAN PHONE NUMBER IS :- " . $_POST['number']."<br>";
+    echo " THE MESSAGE WHICH HE/SHE SENDS  :- ".$_POST['Message']."<br>";
+     ?>
